39ba1902f14535bc3f2e70c0418dd932622d8d41
cbsouthwestern <clintonbrown21@gmail.com>
2024-08-05 16:28:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const images = [
         'https://i.etsystatic.com/15020412/r/il/455abc/2328156575/il_570xN.2328156575_4m7l.jpg',
-        'https://example.com/image2.jpg',
-        'https://example.com/image3.jpg',
+        'https://pbs.twimg.com/media/BYk0HoNCcAAsZVs?format=png&name=small',
+        'https://ih1.redbubble.net/image.4692247109.2389/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u4.jpg'
+        'https://imgix.ranker.com/user_node_img/50016/1000309222/original/birdhair-cage-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375'
         // Add more image URLs here
     ];
 
